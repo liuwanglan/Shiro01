36ff0e762fb3d69648c6509acae51051955f4813
liuwanglan <3429406799@qq.com>
2020-12-18 10:26:50 +0800

    ok

--- src/main/java/com/zking/shiro01/CommonuUtils.java
+++ src/main/java/com/zking/shiro01/CommonuUtils.java
@@ -6,6 +6,11 @@ public class CommonuUtils {
 
     public static void main(String[] args) {
 
+
+        int a=10;
+        int b=12;
+        System.out.println(a+b);
+
         String s = UUID.randomUUID().toString();
         System.out.println(s);
 
