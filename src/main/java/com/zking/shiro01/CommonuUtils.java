b3407e5502ad017a0dc3bd87ce6971f7870fa73b
liuwanglan <3429406799@qq.com>
2020-12-18 10:23:18 +0800

    ok

--- src/main/java/com/zking/shiro01/CommonuUtils.java
+++ src/main/java/com/zking/shiro01/CommonuUtils.java
@@ -1,9 +1,13 @@
 package com.zking.shiro01;
 
+import java.util.UUID;
+
 public class CommonuUtils {
 
     public static void main(String[] args) {
 
+        String s = UUID.randomUUID().toString();
+        System.out.println(s);
 
         System.out.println(System.currentTimeMillis());
 
